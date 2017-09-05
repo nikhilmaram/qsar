7972564e735b4636fb4bcc49c683381bbb756ed1
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2017-09-05 16:52:46 -0700

    test and vega works; store temp result and json in history/md5

--- switch.py
+++ switch.py
@@ -15,7 +15,12 @@ from vega_file import Call_VEGA
 import subprocess
 import re
 import uuid 
+import hashlib
+from episuite_file.parse_episuite import read_epi_result_toJson
+from vega_file.parse_vega import read_vega_result_toJSON
+from test_file.Call_TEST import TEST_batch_allEndpoints
 
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def readJSON(jsonFilePath):
     with open(jsonFilePath) as jsonFile:
@@ -307,71 +312,128 @@ veganone={
         "Skin Sensitization model (CAESAR) - experimental value": "N/A",
         "Skin Sensitization model (CAESAR) - prediction": "N/A"}
 
-def change_vega_script(vega_script_path):
+def change_vega_script(vega_script_template,vega_file_folder,vega_script_path):
     script = None
-    with open(vega_script_path,'r') as f:
-        script = f.readlines()
-        for index in range(len(script)):
-            # print("new line ")
-            # print("orig",script[index])
-            # time.sleep(2)
-            script[index] = re.sub(r'<source>.*</source>','<source>'+os.path.abspath(os.path.normpath('./vega_file/source_test.txt'))+'</source>',script[index])            
-            # print(script[index])
-            # time.sleep(2)            
-            script[index] = re.sub(r'<singleTXT>.*</singleTXT>','<singleTXT>'+os.path.abspath(os.path.normpath('./vega_file/result_test.txt'))+'</singleTXT>',script[index])
-            # print(script[index])          
-            # time.sleep(2)              
-            script[index] = re.sub(r'<multipleTXT>.*</multipleTXT>','<multipleTXT>'+os.path.abspath(os.path.normpath('./vega_file/results'))+'</multipleTXT>',script[index])
-            # print("new",script[index])          
-            # time.sleep(2)       
+    vega_singel_result_path = os.path.abspath(os.path.normpath('{0}/vega_result.txt').format(vega_file_folder))
+    vega_multiple_result_path = os.path.abspath(os.path.normpath('{0}/results').format(vega_file_folder))    
+    with open(vega_script_template,'r') as f:
+        script = f.read()
+
+        script = re.sub(r'<source>.*</source>','<source>'+
+                        os.path.abspath(os.path.normpath('{0}/vega_source.txt').format(vega_file_folder))
+                        +'</source>',script)
+
+        script = re.sub(r'<singleTXT>.*</singleTXT>','<singleTXT>' +
+                        vega_singel_result_path + '</singleTXT>',script)
+        
+        script = re.sub(r'<multipleTXT>.*</multipleTXT>','<multipleTXT>' +
+                        vega_multiple_result_path + '</multipleTXT>',script)
+    # print(script)
+    make_dir_if_necessary(os.path.abspath(os.path.normpath('{0}/results').format(vega_file_folder)))
     with open(vega_script_path,'w') as f:
         f.write("".join(script))
-    print("script changed")
+    
+    return vega_singel_result_path
+    # print("script changed")
     # <singleTXT>/home/awsgui/Desktop/qsar/vega_file/result_test.txt</singleTXT>
     # <multipleTXT>/home/awsgui/Desktop/qsar/vega_file/results</multipleTXT>
-# smile: string, epi,vega,test: true and false switch
-def switch(smile,epi,vega,test,UUID,testopt=1):
-    # give a "hash" for each run of qsar
-    # UUID = str(uuid.uuid1())
-    
-    
 
-    print(dir_path)
-    text_file = open(os.path.join(dir_path, "vega_file/source_test.txt"), "w")	
-    text_file.write(smile+"\n")
-    text_file.close()
-    
-    make_dir_if_necessary(os.path.join(dir_path, "test_file/for_testing/smiles"))
+def serialize_smiles_and_generate_scripts(smiles,temp_dir_path,epi,vega,test):
+    VEGA_SCRIPT_PATH = ""
+    VEGA_RESULT_PATH = ""
+    TEST_SMILES_PATH = ""
+    TEST_RESULT_PATH = ""
+    '''
+    Our episuite script always runs in batch mode. In batch mode, we need
+    feed at least 1 'correct' smiles to episuite so that it can generate output.
+    Thus, to handle potential 'wrong' smiles, we put a place holder in epi_smiles.txt
+    '''
+    if test:
+        # serialize smiles for EPI-suite
+        epi_file_path = os.path.join(temp_dir_path, "episuite_file")
+        make_dir_if_necessary(epi_file_path)
+        epi_smiles = open(os.path.join(epi_file_path,"epi_smiles.txt"), "w")
+        epi_smiles.write("CC\n"+smiles+"\n")
+        epi_smiles.close()
+        # modify sikulix script
 
-    text_file = open(os.path.join(dir_path, "test_file/for_testing/smiles","smiles_{0}.txt".format(UUID)), "w")
-    text_file.write(smile+"\n")
-    text_file.close()
+    '''
+        We could let epi, vega and test read the same input file. However,
+        we think seperating input files and making them more indenpent
+        is a better choice.
+        Also, epi input file has an additional placeholder, 
+        but vega/test does NOT need a placeholder)
+    '''
+    if vega:
+        # serialize smiles for VEGA
+        vega_file_folder = os.path.join(temp_dir_path, "vega_file")
+        make_dir_if_necessary(vega_file_folder)
+        vega_smiles = open(os.path.join(vega_file_folder,'vega_source.txt'), "w")
+        vega_smiles.write(smiles+"\n")
+        vega_smiles.close()
+        # generate VEGA script from template and save the script in temp folder
+        VEGA_SCRIPT_PATH = os.path.join(temp_dir_path,'vega_file/vega_script')
+        VEGA_RESULT_PATH = change_vega_script(os.path.normpath(DIR_PATH + "/vega_file/script_shaoyi"),
+                           vega_file_folder, VEGA_SCRIPT_PATH)
 
-    text_file = open(os.path.join(dir_path, "episuite_file/epi_smiles.txt"), "w")
-    # place holder for batch mode	
-    text_file.write(smile+"\n")
-    text_file.close()
+    if test:
+        # serialize smiles for TEST
+        test_file_path = os.path.join(temp_dir_path, "test_file")
+        make_dir_if_necessary(test_file_path)
+        TEST_RESULT_PATH = test_file_path
+        TEST_SMILES_PATH = os.path.join(test_file_path,"test_smiles.txt")
+        test_smiles = open(TEST_SMILES_PATH, "w")
+        test_smiles.write(smiles+"\n")
+        test_smiles.close()
+
+    return {"VEGA_SCRIPT_PATH":VEGA_SCRIPT_PATH,
+            "VEGA_RESULT_PATH":VEGA_RESULT_PATH,
+            "TEST_SMILES_PATH":TEST_SMILES_PATH,
+            "TEST_RESULT_PATH":TEST_RESULT_PATH}
+
+# smile: string, epi,vega,test: boolean, 
+def switch(smiles,epi,vega,test,UUID,testopt="1"):
+
+    # Given a MD5 hash for a smiles, create a folder that stores epi_script, vega_script
+    # and temperory result for all 3 models. A MD5 is needed because smiles contain special
+    # We can't use smiles as folder identifier directly because:
+    #   1. We are not certain that smiles have a limited length (<255)
+    #   2. EPI might modifies smiles, making it harder to combine results
+    # Thus, we apply MD5 function upon smiles string to obtain a 'unique' identifier
+
+    md5 = hashlib.md5()
+    md5.update(sys.argv[1])
+    SMILES_MD5 = md5.hexdigest()
     
     print(epi,vega,test)
+    print(testopt,type(testopt))
+    # create temporary directory for current smiles
+    # TEMP_DIR_PATH: runtime temp files
+    # RESULT_JSON_FOLDER: 1. json for each model 2. combined json
+    TEMP_DIR_PATH = os.path.join(DIR_PATH,'history',SMILES_MD5)
+    make_dir_if_necessary(TEMP_DIR_PATH)
+    RESULT_JSON_FOLDER = os.path.join(TEMP_DIR_PATH,'json')
+    make_dir_if_necessary(RESULT_JSON_FOLDER)
+    PATH_DICT = serialize_smiles_and_generate_scripts(smiles,TEMP_DIR_PATH,epi,vega,test)
 
     if epi:
         # run sikulix script to operate epi
-        #epi = QSARmod()
-        #epi.run(input_hash={"smiles_in":smile})
         epi_time = time.time()
-        #Call_VEGA("")
-        os.system("/home/awsgui/Desktop/sikulix/runsikulix -r /home/awsgui/Desktop/qsar/sikuli_scripts/epi_script.sikuli")
-        os.system("python " +dir_path+ "/episuite_file/parse_episuite.py")
+        # This command assumes a symlink to runsikulix have been created
+        os.system("{0} -r {1}".format(os.path.join(DIR_PATH,"runsikulix"),
+                                      os.path.join(DIR_PATH,"sikuli_scripts/epi_script.sikuli")))
+        read_epi_result_toJson(EPI_SUITE_SAMPLE_RESULTS_JSON_FILEPATH)
+        # os.system("python " +DIR_PATH+ "/episuite_file/parse_episuite.py")
         #os.system("rm "+dir_path+"/episuite_file/epibat.out")
         print("EPI used {} seconds to complete.".format(time.time()-epi_time))
         #pass
     else:
         currentepi=epinone
-        currentepi["SMILES"]=smile
+        currentepi["SMILES"]=smiles
 
         resultJsonObject=[currentepi]
 
-        jsonOutputPath = os.path.normpath(os.path.join(dir_path,"episuite_file/epibat.json"))
+        jsonOutputPath = os.path.normpath(os.path.join(DIR_PATH,"episuite_file/epibat.json"))
         with open(jsonOutputPath, "w") as outputFile:
             json.dump(resultJsonObject, outputFile,
                     sort_keys=True, indent= 4, separators=(',', ': '))
@@ -380,23 +442,25 @@ def switch(smile,epi,vega,test,UUID,testopt=1):
     if vega:
         vega_time = time.time()
         #os.system("java -jar ./vega_file/VEGA_CMD/VEGA-CLI.jar -script ./vega_file/script_allModule_test")
-        vega_script_path = os.path.normpath(dir_path + "/vega_file/script_shaoyi")
-        java_command = os.path.normpath("{0}/vega_file/VEGA_CMD/VEGA-CLI.jar -script {1}".format(dir_path,vega_script_path))
+        # vega_script_path = os.path.normpath(DIR_PATH + "/vega_file/script_shaoyi")
+        # change_vega_script(vega_script_path)    
+        java_command = os.path.normpath("{0}/vega_file/VEGA_CMD/VEGA-CLI.jar -script {1}"
+                                        .format(DIR_PATH,PATH_DICT['VEGA_SCRIPT_PATH']))
         # change the hard coded path in vega script
-        change_vega_script(vega_script_path)
         os.system("java -jar " + java_command)
-        os.system("python " + os.path.normpath(dir_path+ "/vega_file/parse_vega.py"))
+        read_vega_result_toJSON(PATH_DICT["VEGA_RESULT_PATH"],os.path.join(RESULT_JSON_FOLDER,'vega_result.json'))
+        # os.system("python " + os.path.normpath(DIR_PATH+ "/vega_file/parse_vega.py"))
         print("VEGA used {} seconds to complete.".format(time.time()-vega_time))
         #print("{} process used".format(cpu_count()))
     else:
         #create the empty vage component if switch is off
         currentvega=veganone
-        currentvega["SMILES"]=smile
+        currentvega["SMILES"]=smiles
 
         resultJsonObject = [currentvega]
         
         #output the result
-        jsonOutputPath = os.path.normpath(os.path.join(dir_path,"vega_file/result_test.json"))
+        jsonOutputPath = os.path.normpath(os.path.join(DIR_PATH,"vega_file/result_test.json"))
         with open(jsonOutputPath, "w") as outputFile:
             json.dump(resultJsonObject, outputFile,
                     sort_keys=True, indent= 4, separators=(',', ': '))
@@ -407,13 +471,15 @@ def switch(smile,epi,vega,test,UUID,testopt=1):
 
     if test:
         # print(test_time)
-        # print(os.system("rm -rf {0}".format(os.path.normpath(dir_path+"/test_file/for_testing/temp_result2"))))
+        # print(os.system("rm -rf {0}".format(os.path.normpath(DIR_PATH+"/test_file/for_testing/temp_result2"))))
         # try:
-        command = "python " + os.path.normpath(dir_path + "/test_file/Call_TEST.py ") \
-                  + str(testopt) + " " + str(UUID)
-        print(command)
-        subprocess.call(command,shell=True)
-        # subprocess.check_output(command, stderr=subprocess.STDOUT,shell=True)
+        # command = "python " + os.path.normpath(DIR_PATH + "/test_file/Call_TEST.py ") \
+        #           + str(testopt)
+        TEST_batch_allEndpoints(PATH_DICT["TEST_SMILES_PATH"],PATH_DICT["TEST_RESULT_PATH"],testopt)
+        # print(command)
+        # # subprocess.call(command,shell=True)
+        # try:
+        #     print(subprocess.check_output(command, stderr=subprocess.STDOUT,shell=True))
         # except subprocess.CalledProcessError as exc:
         #     print(exc.output)
         print("{0} process used".format(cpu_count()))
@@ -425,12 +491,12 @@ def switch(smile,epi,vega,test,UUID,testopt=1):
         for a in TEST_Headers:
             currenttest[a]="N/A"
 
-        currenttest["Smiles"]=smile
+        currenttest["Smiles"]=smiles
 
         resultJsonObject = [currenttest]
 
         #output the result
-        jsonOutputPath = os.path.normpath(os.path.join(dir_path,"test_file/for_testing/temp_result2/test_results.json"))
+        jsonOutputPath = os.path.normpath(os.path.join(DIR_PATH,"test_file/for_testing/temp_result2/test_results.json"))
         with open(jsonOutputPath, "w") as outputFile:
             # json.dump(resultJsonObject, outputFile,
             #         sort_keys=True, indent= 4, separators=(',', ': '))
@@ -441,10 +507,15 @@ def switch(smile,epi,vega,test,UUID,testopt=1):
 
     #epiJSON = readJSON(EPI_SUITE_SAMPLE_RESULTS_JSON_FILEPATH)
     #vegaJSON = readJSON(VEGA_SAMPLE_RESULTS_JSON_FILEPATH)
-    print(smile)
-    #testJSON = readJSON(TEST_SAMPLE_RESULTS_JSON_FILEPATH)
+    print(smiles)
+    # testJSON = readJSON(TEST_SAMPLE_RESULTS_JSON_FILEPATH)
     # print(testJSON)
     # return testJSON
+
+    #outputFilePath = DEFAULT_JSON_OUTPUT_FILEPATH
+    #qsar_dict = parse(epiJSON,vegaJSON,testJSON,outputFilePath)
+    #print(qsar_dict)
+    #return qsar_dict
 
 def save_test_result(outfile_path):
     # append output json of current smiles to the large json of 500 smiles
@@ -452,23 +523,22 @@ def save_test_result(outfile_path):
         with open(TEST_SAMPLE_RESULTS_JSON_FILEPATH,'r') as fp_in:
             fp_out.write(fp_in.read())
 
-    #outputFilePath = DEFAULT_JSON_OUTPUT_FILEPATH
-    #qsar_dict = parse(epiJSON,vegaJSON,testJSON,outputFilePath)
-    #print(qsar_dict)
-    #return qsar_dict
+def save_epi_result(outfile_path):
+    with open(outfile_path,'a') as fp_out:
+        with open(EPI_SUITE_SAMPLE_RESULTS_JSON_FILEPATH,'r') as fp_in:
+            fp_out.write(fp_in.read())        
 
 if __name__ == '__main__':
     #switch("C(Cl)Cl",True,True,False)
     #testopt, 1:all,0:density and orat
     # print(len(sys.argv),sys.argv)    
-    UUID = sys.argv[5]
+    # UUID = sys.argv[5]
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    EPI_SUITE_SAMPLE_RESULTS_JSON_FILEPATH= os.path.normpath(dir_path + "/episuite_file/epibat.json")
-    VEGA_SAMPLE_RESULTS_JSON_FILEPATH = os.path.normpath(dir_path + "/vega_file/result_test.json")
-    TEST_SAMPLE_RESULTS_JSON_FILEPATH =  os.path.normpath(os.path.join(dir_path + "/test_file/for_testing/temp_result_" + UUID + "/test_results.json"))
-    DEFAULT_JSON_OUTPUT_FILEPATH =  os.path.normpath(dir_path + "/QSAR_summay_sample.json")
+    EPI_SUITE_SAMPLE_RESULTS_JSON_FILEPATH = os.path.normpath(DIR_PATH + "/episuite_file/epibat.json")
+    # VEGA_SAMPLE_RESULTS_JSON_FILEPATH = os.path.normpath(DIR_PATH + "/vega_file/result_test.json")
+    # TEST_SAMPLE_RESULTS_JSON_FILEPATH =  os.path.normpath(os.path.join(DIR_PATH + "/test_file/for_testing/temp_result_" + UUID + "/test_results.json"))
+    # DEFAULT_JSON_OUTPUT_FILEPATH =  os.path.normpath(DIR_PATH + "/QSAR_summay_sample.json")
 
     if len(sys.argv) == 7:
         switch(sys.argv[1],eval(sys.argv[2]),eval(sys.argv[3]),eval(sys.argv[4]),sys.argv[5],sys.argv[6])
@@ -478,7 +548,11 @@ if __name__ == '__main__':
         switch(sys.argv[1],eval(sys.argv[2]),eval(sys.argv[3]),eval(sys.argv[4]),sys.argv[5],sys.argv[6])
         print("switch finished")
         save_test_result(sys.argv[7])
-        # python switch.py CC False False True UUID 0
+    
+    # production
+    if len(sys.argv) == 5:
+        switch(sys.argv[1],eval(sys.argv[2]),eval(sys.argv[3]),eval(sys.argv[4]),'uuid',"1")
+        # python switch.py CC False True False
     #switch("CC",False,True,False,test_opt)
     #switch("C(Cl)Cl",True,False,False,test_opt)
 
